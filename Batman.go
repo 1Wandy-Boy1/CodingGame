6ff3878baec09bc47676b1f579b31e2de7aa7c2f
SSpeadyHighPriestCoding <71380116+neos53xbox@users.noreply.github.com>
2020-11-23 04:01:17 +0100

    Add files via upload

---
+++ Batman.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var W, H int
+	fmt.Scan(&W, &H)
+	var N int
+	fmt.Scan(&N)
+	var X0, Y0 int
+	fmt.Scan(&X0, &Y0)
+
+	var X1, Y1 int
+
+	var X2 int
+	X2 = W - 1
+
+	var Y2 int
+	Y2 = H - 1
+	for {
+		var bombDir string
+		fmt.Scan(&bombDir)
+		if strings.ContainsAny(bombDir, "U") == true {
+			Y2 = Y0 - 1
+		} else if strings.ContainsAny(bombDir, "D") == true {
+			Y1 = Y0 + 1
+		}
+
+		if strings.ContainsAny(bombDir, "L") == true {
+			X2 = X0 - 1
+		} else if strings.ContainsAny(bombDir, "R") == true {
+			X1 = X0 + 1
+		}
+		X0 = X1 + (X2-X1)/2
+		Y0 = Y1 + (Y2-Y1)/2
+
+		fmt.Println(X0, Y0)
+	}
+}
